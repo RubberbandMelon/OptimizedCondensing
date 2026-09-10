d47cde2f09a93a90825797e673fa91254b39cca0
C-mon-K <c.mon@alb.one>
2026-09-10 22:27:34 +0200

    changed pins and channels to subrack assignment

--- execute.py
+++ execute.py
@@ -21,10 +21,16 @@ settings = {
     'VALVE_SORB_CHANNEL' : 6,
     'VALVE_SORB_PULSE' : 17,
     'VALVE_SORB_DIR' : 27,
+    'VALVE_SORB_ENA' : 22,
     'VALVE_1K_CHANNEL' : 5,
-    'VALVE_1K_PULSE' : 23,
-    'VALVE_1K_DIR' :  24,
+    'VALVE_1K_PULSE' : 5,
+    'VALVE_1K_DIR' :  6,
+    'VALVE_1K_ENA' : 13,
     'ADC_ADDRESSES' : (0x68, 0x68),
+    'POTI_POWER' : 19,
+
+    'VALVE_SORB_CHANNEL' : 7,
+    'VALVE_1K_CHANNEL' : 8,
 
     'VALVE_SORB_OPEN' : 2,
     'VALVE_SORB_CLOSED' : 0.2,
